9fcea7a443b9bcebfafb5113040ae724ba2b9c17
Dominic-wells <joedomwells@gmail.com>
2022-10-20 16:35:40 +0100

    Added pages for site /about/index/contact

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,8 +8,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
+@app.route("/about")    
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(
